5799081c5b72f76a4256169fbb816a2815246dd7
Rashika Rathi <b18081@students.iitmandi.ac.in>
2021-05-25 11:41:00 +0530

    Added bootstrap icons

--- src/index.js
+++ src/index.js
@@ -31,18 +31,16 @@ const App = () => {
         <div class="transbox">
           <h1>Breaking Bad Characters</h1>
           <div class="img1">
-            {" "}
             <img
               src="https://avatars1.githubusercontent.com/u/45493793?s=460&amp;u=81836d78246a7e3c0724635018ab93450d6ba9e6&amp;v=4"
               alt="Rashika Rathi"
               width="80"
             ></img>
           </div>
-          <p></p>
           <h3>
             by
             <a href="https://www.facebook.com/rashika.sinha.75">
-              <div class="img2">Rashika Rathi</div>
+              <br></br>Rashika Rathi
             </a>
           </h3>
         </div>
@@ -62,7 +60,9 @@ const App = () => {
               placeholder="Type here.."
               name="search"
             ></input>
-            <button type="button" class="btn btn-default">Search</button>
+            <button type="button" class="btn btn-default">
+              <i class="bi bi-search"></i>
+            </button>
           </form>
         </div>
       </div>
@@ -77,77 +77,46 @@ const App = () => {
       <footer id="contact">
         <h2>Contact Me</h2>
         <address>
-          {" "}
           Rashika Rathi<br></br>
           <a
             href="https://www.google.com/maps/place/B20+Girls'+Hostel/@31.7813298,76.9950323,17.53z/data=!4m12!1m6!3m5!1s0x3904e4a66418174b:0x109b52bfc879ec97!2sBeas+Kund+Hostel,+IIT+Mandi+North+Campus!8m2!3d31.7811608!4d76.9964329!3m4!1s0x3904e4a67bb7c23d:0x114604ecfc1285a8!8m2!3d31.7814888!4d76.9962014"
             target="_blank"
-            rel="noopener"
+            rel="noopener noreferrer"
           >
-            {" "}
-            B18081,B16<br></br>IIT Mandi,India{" "}
+            B18081, B16<br></br>IIT Mandi, India
           </a>
           <br></br> +91-9113322022
         </address>
         <a
           href="https://www.facebook.com/rashika.sinha.75"
           target="_blank"
-          rel="noopener"
+          rel="noopener noreferrer"
         >
-          <img
-            src="https://raw.githubusercontent.com/raashika03/webtrial/master/social_media_logos/s1.png"
-            width="20"
-            height="20"
-            alt="fb"
-          ></img>
+          <i class="bi bi-facebook"></i>
         </a>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <a href="https://twitter.com/raashika03" target="_blank" rel="noopener">
-          <img
-            src="https://raw.githubusercontent.com/raashika03/webtrial/master/social_media_logos/s2.webp"
-            width="20"
-            height="20"
-            alt="twitter"
-          ></img>
+        <a
+          href="https://twitter.com/raashika03"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="bi bi-twitter"></i>
         </a>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <a
           href="https://www.instagram.com/raashika.03/"
           target="_blank"
-          rel="noopener"
+          rel="noopener noreferrer"
         >
-          <img
-            src="https://raw.githubusercontent.com/raashika03/webtrial/master/social_media_logos/s3.png"
-            width="20"
-            height="20"
-            alt="IG"
-          ></img>
+          <i class="bi bi-instagram"></i>
         </a>
-        <br></br>
-        <a
-          href="https://www.pinterest.ca/rashikasinha32/"
-          target="_blank"
-          rel="noopener"
-        >
-          <img
-            src="https://raw.githubusercontent.com/raashika03/webtrial/master/social_media_logos/s4.png"
-            width="20"
-            height="20"
-            alt="PI"
-          ></img>
-        </a>
-        &nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <a
           href="https://www.linkedin.com/in/rashika-rathi-65909a174/"
           target="_blank"
-          rel="noopener"
+          rel="noopener noreferrer"
         >
-          <img
-            src="https://raw.githubusercontent.com/raashika03/webtrial/master/social_media_logos/s5.jpg"
-            width="20"
-            height="20"
-            alt="in"
-          ></img>
+          <i class="bi bi-linkedin"></i>
         </a>
         <br></br>
       </footer>
